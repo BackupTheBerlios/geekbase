3e6c8a3c3f2a4e0391ac7bbe272d99b3be2aa77a
algidus <algidus>
2005-01-28 08:38:53 +0000

    Added enums.h

---
+++ geekbase/enums.h
@@ -0,0 +1,10 @@
+/* Enums */
+enum field_type {
+	TYPE_INT,       /**< field is an integers collector */
+	TYPE_STRING,    /**< field is a strings collector */
+	TYPE_TIMESTAMP  /**< field is a timestamp (xx:yy:zz) collector */
+};
+
+enum env_type {
+	TYPE_DISK  /**<  */
+};
